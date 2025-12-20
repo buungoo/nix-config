fd0cbc6442bd8ad79a8adae854eee4c1008b0db6
Alex Bergdahl <bergdahlalex@protonmail.com>
2025-12-20 20:46:29 +0100

    determine ssh nas0 from wireguard connectivity

--- home/bungo/common/core/ssh.nix
+++ home/bungo/common/core/ssh.nix
@@ -1,7 +1,13 @@
 {
   pkgs,
+  inputs,
   ...
 }:
+let
+  nas0Config = inputs.nix-secrets.nas0;
+  wireguardIP = nas0Config.networking.wireguardIP;
+  lanIP = nas0Config.networking.localIP;
+in
 {
   programs.ssh = {
     enable = true;
@@ -13,6 +19,14 @@
         user = "git";
         identitiesOnly = true;
         identityFile = "~/.ssh/id_ed25519";
+      };
+
+      "nas0" = {
+        user = "bungo";
+        identityFile = "~/.ssh/id_ed25519";
+        # Try Wireguard IP first, fall back to LAN IP
+        # nc -z tests connectivity with 1 second timeout
+        proxyCommand = "sh -c '${pkgs.netcat}/bin/nc -z -w1 ${wireguardIP} 22 2>/dev/null && exec ${pkgs.netcat}/bin/nc ${wireguardIP} 22 || exec ${pkgs.netcat}/bin/nc ${lanIP} 22'";
       };
     };
   };
